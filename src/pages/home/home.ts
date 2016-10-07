058a7a2d00f276e39ebbe5707264443b1dbf48a5
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-10-07 18:03:17 +0300

    remove the action sheet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,32 +8,8 @@ import { ActionSheetController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
+  constructor(public navCtrl: NavController) {
 
-  }
-
-  presentActionSheet(): void {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'title',
-      buttons: [
-        {
-          text: 'Button 1',
-          role: 'button1',
-          handler: () => {
-            console.log('Button 1 clicked!');
-          }
-        },
-        {
-          text: 'Button 2',
-          role: 'button2',
-          handler: () => {
-            console.log('Button 2 clicked!')
-          }
-        }
-      ]
-    });
-
-    actionSheet.present();
   }
 
 }
