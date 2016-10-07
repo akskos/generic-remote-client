cbf8c7a18ab9850a5773aac608d25d37a009a60a
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-10-07 18:21:25 +0300

    add settings page and navigate to it with a button in homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SettingsPage } from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SettingsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SettingsPage
   ],
   providers: []
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { SettingsPage } from '../settings/settings';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,6 +12,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoSettings() {
+    this.navCtrl.push(SettingsPage);
   }
 
 }
